cc51f390980a3b4d14294eda9adb350262bc7fe4
mitto <mitto.hai.7356@gmail.com>
2022-02-23 17:28:43 +0700

    first commit

---
+++ HungHaStore/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace HungHaStore
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.3.1.min.js",
+                        "~/Scripts/jquery.nice-select.min.js",
+                        "~/Scripts/jquery-ui.min.js",
+                        "~/Scripts/jquery.slicknav.js",
+                        "~/Scripts/mixitup.min.js",
+                        "~/Scripts/owl.carousel.min.js",
+                        "~/Scripts/main.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+            "~/Scripts/main.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/elegant-icons.css",
+                      "~/Content/nice-select.css",
+                      "~/Content/jquery-ui.min.css",
+                      "~/Content/owl.carousel.min.css",
+                      "~/Content/slicknav.min.css",
+                      "~/Content/style.css"));
+
+        }
+    }
+}
