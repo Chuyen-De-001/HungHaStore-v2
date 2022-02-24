6aed2606429d739a12df3ddf80b4294a9a8fcd20
HaiPV <mitto.hai.7356@gmail.com>
2022-02-25 02:58:41 +0700

    update template

--- HungHaStore/Controllers/ProductController.cs
+++ HungHaStore/Controllers/ProductController.cs
@@ -13,5 +13,10 @@ namespace HungHaStore.Controllers
         {
             return View();
         }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
     }
 }
