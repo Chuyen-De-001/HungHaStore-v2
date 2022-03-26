21d9df2a12829dbcfbcaa5f213c2c8bcecd33dbd
mitto <mitto.hai.7356@gmail.com>
2022-03-27 00:53:15 +0700

    turn of search khach hang

--- HungHaStore/Areas/Admin/Controllers/AUserController.cs
+++ HungHaStore/Areas/Admin/Controllers/AUserController.cs
@@ -19,10 +19,6 @@ namespace HungHaStore.Areas.Admin.Controllers
         public ActionResult Index(int pageSize = 20, int page = 1,string search = "")
         {
             var query = db.nguoi_dung.OrderByDescending(s => s.id);
-            if(search != "")
-            {
-                query = (IOrderedQueryable<nguoi_dung>)query.Where(s => s.ho_ten.Contains(search) || s.email.Contains(search) || s.sdt.Contains(search) || s.tai_khoan.Contains(search));
-            }
             IEnumerable<nguoi_dung> nguoiDungs = query.ToPagedList(page, pageSize);
             ViewBag.search = search;
             return View(nguoiDungs);
@@ -47,38 +43,6 @@ namespace HungHaStore.Areas.Admin.Controllers
         public ActionResult Create()
         {
             return View();
-        }
-
-        // POST: Admin/AUser/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,tai_khoan,mat_khau,email,ngay_sinh,sdt,dia_chi,gioi_tinh,quyen,tg_tao")] nguoi_dung nguoi_dung)
-        {
-            if (ModelState.IsValid)
-            {
-                db.nguoi_dung.Add(nguoi_dung);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(nguoi_dung);
-        }
-
-        // GET: Admin/AUser/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            nguoi_dung nguoi_dung = db.nguoi_dung.Find(id);
-            if (nguoi_dung == null)
-            {
-                return HttpNotFound();
-            }
-            return View(nguoi_dung);
         }
 
         // POST: Admin/AUser/Edit/5
